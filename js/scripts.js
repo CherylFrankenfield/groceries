c7fcadecde08fe59b1a6f24aaf888b613459ae8d
Cheryl Frankenfield and Lena Ha <lena@>
2017-10-23 16:09:29 -0700

    make mapping loops to capitalize and sort

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,33 @@
 $(document).ready(function() {
   $("form#groceriesForm").submit(function(event) {
     var groceries = ["list0", "list1", "list2", "list3", "list4", "list5", "list6", "list7", "list8", "list9"];
-    var luke = [];
+    var things = [];
 
     groceries.forEach(function(grocery) {
       var input = $("input#" + grocery).val();
       if (input) {
-        luke.push(input);
+        things.push(input);
       }
     });
 
-    //else: there is input, push into array
-      //newarray = array.sort()
-      //newarray.toUpperCase()
-      //<ul> for the sorted list items of newarray.toUpperCase()
+      things.sort();
 
+      var purchases = [];
 
-    // var inputs = [input#list1, input#list2, input#list3, input#list4, input#list5, input#list6, input#list7, input#list8, input#list9,input#list10]
-    //
-    // alert(inputs)
+      var purchases = things.map(function(thing) {
+        thing = thing.toUpperCase();
+        return thing
+      });
 
-    alert(luke)
+      var finals = [];
+
+      var finals = purchases.map(function(final) {
+        final = "<li>" + final + "</li>";
+        return final
+      });
+
+    $("#showList").append(finals);
+    $("#groceriesForm").hide();
     event.preventDefault();
   });
 });
