0e206958b6ff04ba55dd234fd3f97748f81f46a1
Cheryl Frankenfield and Lena Ha <lena@>
2017-10-23 14:01:28 -0700

    create a grocery list form and put input into array

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".submit-btn").click(function(event) {
+    var inputs = ["input#list1", "input#list2", "input#list3", "input#list4", "input#list5", "input#list6", "input#list7", "input#list8", "input#list9","input#list10"]
+    event.preventDefault();
+  });
+});
